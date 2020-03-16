f9ec174e79a78e244e223f681caa781ec840d500
Mark <marktoh96@gmail.com>
2020-03-16 00:56:50 -0500

    Updates

--- precache-manifest.20240f41fd93f2d82199d1311b3af369.js
+++ precache-manifest.20240f41fd93f2d82199d1311b3af369.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6f13e3e033067726bb2a5235d11092e0",
+    "revision": "a0e886c7fae2b43f88df5200f52e742e",
     "url": "/open-f1/index.html"
   },
   {
-    "revision": "f029c654a9f824e7d672",
+    "revision": "09377798f5a4dbe447ca",
     "url": "/open-f1/static/css/main.d82977bf.chunk.css"
   },
   {
-    "revision": "7d0c2e7f619548cbf8e0",
-    "url": "/open-f1/static/js/2.3f43ab0d.chunk.js"
+    "revision": "fd370b7bd5abb139f633",
+    "url": "/open-f1/static/js/2.a0445b01.chunk.js"
   },
   {
     "revision": "8c7fe4041954829e8f9a44c5426cd717",
-    "url": "/open-f1/static/js/2.3f43ab0d.chunk.js.LICENSE.txt"
+    "url": "/open-f1/static/js/2.a0445b01.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f029c654a9f824e7d672",
-    "url": "/open-f1/static/js/main.af03ecb5.chunk.js"
+    "revision": "09377798f5a4dbe447ca",
+    "url": "/open-f1/static/js/main.2e723ed0.chunk.js"
   },
   {
     "revision": "591d664707dcf4f08985",
